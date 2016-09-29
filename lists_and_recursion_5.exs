365e72c8f006a7a13ffb2830d431bbf63f90ac59
Sid Ngeth <sngeth@gmail.com>
2016-09-29 09:15:37 -0400

    Complete #split and #take for ListsAndRecursion-5

--- lists_and_recursion_5.exs
+++ lists_and_recursion_5.exs
@@ -16,11 +16,20 @@ defmodule MyList do
   def each([], _func), do: :ok
 
   def filter([head | tail], func) do
-    if func.(head) == true do
+    if func.(head) do
       [ head | filter(tail, func) ]
     else
       filter(tail, func)
     end
   end
   def filter([], _func), do: []
+
+  def split(list, count),      do: _split(list, [], count)
+  defp _split([], front, _),   do: [ Enum.reverse(front), [] ]
+  defp _split(tail, front, 0), do: [ Enum.reverse(front), tail ]
+  defp _split([ head | tail ], front, count)  do
+    _split(tail, [head|front], count-1)
+  end
+
+  def take(list, n), do: hd(split(list, n))
 end
