616a4c98f62db83beca5ab8d43364e35e714d61b
Sid Ngeth <sngeth@gmail.com>
2017-01-18 10:16:19 -0500

    Solution for StringsAndBinaries-4

--- strings_and_binaries_4.exs
+++ strings_and_binaries_4.exs
@@ -3,3 +3,29 @@
 # numbers do not have a leading plus or minus sign
 # calculate('123 + 27') # => 150
 
+defmodule Parse do
+  def calculate(expression) do
+    { n1, rest } = parse_number(expression)
+    rest         = skip_spaces(rest)
+    { op, rest } = parse_operator(rest)
+    rest         = skip_spaces(rest)
+    { n2, [] }   = parse_number(rest)
+    op.(n1, n2)
+  end
+
+  defp parse_number(expression), do: _parse_number({0, expression})
+
+  defp _parse_number({value, [digit | rest]}) when digit in ?0..?9 do
+    _parse_number({ value*10 + digit - ?0, rest})
+  end
+
+  defp _parse_number(result), do: result
+
+  defp skip_spaces([ ?\s | rest]), do: skip_spaces(rest)
+  defp skip_spaces(rest),          do: rest
+
+  defp parse_operator([ ?+ | rest ]), do: { &(&1+&2), rest }
+  defp parse_operator([ ?- | rest ]), do: { &(&1-&2), rest }
+  defp parse_operator([ ?* | rest ]), do: { &(&1*&2), rest }
+  defp parse_operator([ ?/ | rest ]), do: { &(div(&1,&2)), rest }
+end
