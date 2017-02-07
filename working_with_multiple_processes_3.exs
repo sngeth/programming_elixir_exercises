287963b42456841e34126822de6486e9590dc84e
Sid Ngeth <sngeth@gmail.com>
2017-02-07 13:43:43 -0500

    Complete WorkingWithMultipleProcesses-3

---
+++ working_with_multiple_processes_3.exs
@@ -0,0 +1,30 @@
+# Use spawn_link to start a process, and have that process send a message
+# to the parent and then exit immediately. Meanwhile, sleep for 500 ms in
+# the parent, then receive as many messages as are waiting. Trace what
+# you recieve. Does it matter that you weren't waiting for the notification
+# from the child when it exited?
+
+defmodule Link do
+  def child(parent_pid, exit_result) do
+    send parent_pid, "hello"
+    exit(exit_result)
+  end
+
+  def parent do
+    :timer.sleep(500)
+    receive_all
+  end
+
+  defp receive_all do
+    receive do
+      message -> IO.inspect message
+      receive_all
+    after 1000 ->
+      IO.puts "No more messages"
+    end
+  end
+end
+
+Process.flag(:trap_exit, true)
+spawn_link(Link, :child, [self, :error])
+Link.parent
